97ddd7b4aeb2e70d5c0d5891fd4f5734ce8aae7e
MrFast <mrfastkrunker@gmail.com>
2024-01-24 22:21:58 -0500

    init config and player stats

--- src/main/java/mrfast/sbt/SkyblockTweaks.kt
+++ src/main/java/mrfast/sbt/SkyblockTweaks.kt
@@ -1,13 +1,12 @@
 package mrfast.sbt
 
-import mrfast.sbt.SkyblockTweaks
-import net.minecraftforge.client.event.ClientChatReceivedEvent
+import mrfast.sbt.apis.PlayerStats
+import mrfast.sbt.config.Config
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Mod(modid = SkyblockTweaks.MOD_ID, name = SkyblockTweaks.MOD_NAME, version = SkyblockTweaks.MOD_VERSION)
 class SkyblockTweaks {
@@ -15,17 +14,21 @@ class SkyblockTweaks {
         const val MOD_ID = "skyblocktweaks"
         const val MOD_NAME = "Skyblock Tweaks"
         const val MOD_VERSION = "1.0.0"
+        val config = Config()
     }
 
     @Mod.EventHandler
     fun preInit(event: FMLPreInitializationEvent?) {
-        // Your pre-init code here
-        MinecraftForge.EVENT_BUS.register(this)
+        println("Loading config for SBT")
+        config.load();
     }
 
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent?) {
         // Your init code here
+        MinecraftForge.EVENT_BUS.register(this)
+        // Api's
+        MinecraftForge.EVENT_BUS.register(PlayerStats)
     }
 
     @Mod.EventHandler
