4cd899e9c4050a2bcb8a581c162773bca6d8cafa
MrFast <mrfastkrunker@gmail.com>
2024-01-29 18:31:40 -0500

    Added own clamp method, clipboard copy

--- src/main/java/mrfast/sbt/utils/Utils.kt
+++ src/main/java/mrfast/sbt/utils/Utils.kt
@@ -1,6 +1,8 @@
 package mrfast.sbt.utils
 
 import net.minecraft.client.Minecraft
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 
 object Utils {
     val mc: Minecraft = Minecraft.getMinecraft()
@@ -18,5 +20,15 @@ object Utils {
 
     fun formatNumber(number: Int): String {
         return String.format("%,d", number)
+    }
+
+    fun clamp(min: Double,value: Double,max:Double): Double {
+        return max.coerceAtMost(value.coerceAtLeast(min))
+    }
+
+    fun copyToClipboard(text: String) {
+        val stringSelection = StringSelection(text)
+        val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        clipboard.setContents(stringSelection, null)
     }
 }
