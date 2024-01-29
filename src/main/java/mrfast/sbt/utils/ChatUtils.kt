151afa0db6f50980fa0316a444f83fab1dfb962b
MrFast <mrfastkrunker@gmail.com>
2024-01-29 13:10:41 -0500

    Create chat utils

---
+++ src/main/java/mrfast/sbt/utils/ChatUtils.kt
@@ -0,0 +1,15 @@
+package mrfast.sbt.utils
+
+import com.mojang.realmsclient.gui.ChatFormatting
+import net.minecraft.client.Minecraft
+import net.minecraft.util.ChatComponentText
+
+object ChatUtils {
+    fun sendPlayerMessage(message: String) {
+        Minecraft.getMinecraft().thePlayer.sendChatMessage(message)
+    }
+    fun logMessage(message: String) {
+        val prefix = "§eSkyblock§9Tweaks§6 >>§r "
+        Minecraft.getMinecraft().ingameGUI.chatGUI.printChatMessage(ChatComponentText(prefix+message))
+    }
+}
