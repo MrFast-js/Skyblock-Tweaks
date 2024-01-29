2f650d34bc8837c83b27e9040151491b88e2b705
MrFast <mrfastkrunker@gmail.com>
2024-01-29 18:32:17 -0500

    init scoreboard utils

---
+++ src/main/java/mrfast/sbt/utils/ScoreboardUtils.kt
@@ -0,0 +1,40 @@
+package mrfast.sbt.utils
+
+import net.minecraft.client.Minecraft
+import net.minecraft.scoreboard.Score
+import net.minecraft.scoreboard.ScorePlayerTeam
+
+
+object ScoreboardUtils {
+
+    fun getScoreboardLines(cleanColor: Boolean): List<String> {
+        val scoreboard = Minecraft.getMinecraft().theWorld?.scoreboard
+        val objective = scoreboard?.getObjectiveInDisplaySlot(1)
+
+        return scoreboard?.let {
+            it.getSortedScores(objective)
+                    .map { score: Score ->
+                        val out = score.playerName?.let { playerName ->
+                            stripAlienCharacters(ScorePlayerTeam.formatPlayerName(it.getPlayersTeam(playerName), playerName))
+                        } ?: ""
+
+                        if (cleanColor) Utils.cleanColor(out)
+                        out
+                    }
+                    .reversed()
+        } ?: emptyList()
+    }
+
+    /**
+     * This code is unmodified
+     * @Author: nea98
+     * @Source: https://moddev.nea.moe
+     */
+    fun stripAlienCharacters(text: String): String {
+        val sb = StringBuilder()
+        for (c in text.toCharArray()) {
+            if (Minecraft.getMinecraft().fontRendererObj.getCharWidth(c) > 0 || c == '§') sb.append(c)
+        }
+        return sb.toString()
+    }
+}
