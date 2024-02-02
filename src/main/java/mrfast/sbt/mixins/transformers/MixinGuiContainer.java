ca9159671b3236dee5779a2a9f8bedc7f2af7b62
MrFast <mrfastkrunker@gmail.com>
2024-02-02 00:15:28 -0500

    init mixins

---
+++ src/main/java/mrfast/sbt/mixins/transformers/MixinGuiContainer.java
@@ -0,0 +1,21 @@
+package mrfast.sbt.mixins.transformers;
+
+import mrfast.sbt.customevents.SlotClickedEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Slot;
+import net.minecraftforge.common.MinecraftForge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiContainer.class)
+public class MixinGuiContainer {
+    @Inject(method = "handleMouseClick(Lnet/minecraft/inventory/Slot;III)V", at = @At("HEAD"))
+    private void onHandleMouseClick(Slot slot, int slotId, int clickedButton, int mode, CallbackInfo ci) {
+        if(slot!=null) {
+            MinecraftForge.EVENT_BUS.post(new SlotClickedEvent(slot, (GuiContainer) Minecraft.getMinecraft().currentScreen));
+        }
+    }
+}
