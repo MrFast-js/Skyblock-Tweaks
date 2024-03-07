3d5f6f4116198aadab4b77a6f6f5342904a11a7f
MrFast <mrfastkrunker@gmail.com>
2024-03-07 18:42:27 -0500

    Created Item Gain/Loss events

---
+++ src/main/java/mrfast/sbt/customevents/SkyblockInventoryItemEvent.kt
@@ -0,0 +1,17 @@
+package mrfast.sbt.customevents
+
+import net.minecraft.item.ItemStack
+import net.minecraftforge.fml.common.eventhandler.Event
+
+open class SkyblockInventoryItemEvent : Event() {
+    enum class EventType {
+        GAINED,
+        LOST
+    }
+
+    open class InventoryItemEvent(val eventType: EventType, val amount: Int = 1) : SkyblockInventoryItemEvent()
+
+    class ItemStackEvent(eventType: EventType, amount: Int = 1, val itemStack: ItemStack) : InventoryItemEvent(eventType, amount)
+
+    class SackItemEvent(eventType: EventType, amount: Int = 1, val materialName: String) : InventoryItemEvent(eventType, amount)
+}
