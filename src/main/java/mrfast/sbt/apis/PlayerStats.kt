0e6c090fc9c9386e805bb173191045d2efe2e548
MrFast <mrfastkrunker@gmail.com>
2024-06-25 12:10:54 -0400

    Remove DamageReductionPercentage.kt

--- src/main/java/mrfast/sbt/apis/PlayerStats.kt
+++ src/main/java/mrfast/sbt/apis/PlayerStats.kt
@@ -21,8 +21,6 @@ object PlayerStats {
     var defense = 0
     var effectiveHealth = 0
     var maxEffectiveHealth = 0
-    
-    var damageReduction = 0.0
 
     var maxRiftTime = 0
     var riftTimeSeconds = 0
@@ -85,7 +83,6 @@ object PlayerStats {
         maxHealth = split[1].toInt()
         effectiveHealth = (health * (1 + defense / 100))
         maxEffectiveHealth = (maxHealth * (1 + defense / 100))
-        damageReduction = ((defense * (1.0 + defense + 100.0)) * 100).toInt() / 100.0
         absorption = max(health - maxHealth, 0)
     }
 
