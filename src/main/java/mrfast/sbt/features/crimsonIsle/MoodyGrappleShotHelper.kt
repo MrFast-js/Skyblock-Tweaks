75eda404e3afba65b0c6c37be47f140208cc7df0
MrFast <mrfastkrunker@gmail.com>
2024-08-08 22:36:16 -0400

    Fix wrong config value - MoodyGrappleShotHelper

--- src/main/java/mrfast/sbt/features/crimsonIsle/MoodyGrappleShotHelper.kt
+++ src/main/java/mrfast/sbt/features/crimsonIsle/MoodyGrappleShotHelper.kt
@@ -3,21 +3,17 @@ package mrfast.sbt.features.crimsonIsle
 import mrfast.sbt.SkyblockTweaks
 import mrfast.sbt.apis.SkyblockMobDetector
 import mrfast.sbt.config.categories.CrimsonConfig
-import mrfast.sbt.config.categories.DungeonConfig
 import mrfast.sbt.customevents.RenderEntityOutlineEvent
-import mrfast.sbt.customevents.SkyblockMobEvent
 import mrfast.sbt.utils.ItemUtils.getSkyblockId
 import mrfast.sbt.utils.LocationUtils
 import mrfast.sbt.utils.Utils
-import mrfast.sbt.utils.Utils.clean
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import java.awt.Color
 
 @SkyblockTweaks.EventComponent
 object MoodyGrappleShotHelper {
     @SubscribeEvent
     fun onRenderEntityOutlines(event: RenderEntityOutlineEvent.Normal) {
-        if (LocationUtils.currentIsland != "Crimson Isle" || CrimsonConfig.moodyGrappleShotHighlight) return
+        if (LocationUtils.currentIsland != "Crimson Isle" || !CrimsonConfig.moodyGrappleShotHighlight) return
 
         if(Utils.mc.thePlayer.heldItem?.getSkyblockId() != "MOODY_GRAPPLESHOT") return
 
