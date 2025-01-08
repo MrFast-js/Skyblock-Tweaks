03f579d66abedf50c71df8286ab0f6d1e94eb552
MrFast <mrfastkrunker@gmail.com>
2025-01-08 16:08:49 -0500

    Improve Gmestone Grinder overlay for empty slots, cost to remove, etc

--- src/main/java/mrfast/sbt/config/categories/MiscellaneousConfig.kt
+++ src/main/java/mrfast/sbt/config/categories/MiscellaneousConfig.kt
@@ -76,24 +76,6 @@ object MiscellaneousConfig : Config() {
     )
     var quiverOverlayType = false
 
-    //    @ConfigProperty(
-//            type = ConfigType.TOGGLE,
-//            name = "Fire Veil Timer",
-//            description = "Shows the time until the Fire Veil ability ends.",
-//            category = "Miscellaneous",
-//            subcategory = "Items"
-//    )
-//    var fireVeilTimer = false
-//
-//    @ConfigProperty(
-//            type = ConfigType.TOGGLE,
-//            name = "Prevent §5Gloomlock Grimoire§r Death",
-//            description = "Stops the §5Gloomlock Grimoire's§r ability from killing the player by blocking the ability if §cPlayer Health < 25%",
-//            category = "Miscellaneous",
-//            subcategory = "Items"
-//    )
-//    var gloomlockGrimoireProtection = true
-//
     @ConfigProperty(
         type = ConfigType.TOGGLE,
         name = "Fire Freeze Staff Visual",
@@ -122,56 +104,6 @@ object MiscellaneousConfig : Config() {
     )
     var fireFreezeVisualColor = CustomColor(Color.RED)
 
-//    @ConfigProperty(
-//            type = ConfigType.TOGGLE,
-//            name = "Show Prehistoric Egg Distance Counter",
-//            description = "Shows the blocks walked on the Prehistoric Egg item",
-//            category = "Miscellaneous",
-//            subcategory = "Items"
-//    )
-//    var prehistoricEggDistance = false
-//
-//    @ConfigProperty(
-//            type = ConfigType.TOGGLE,
-//            name = "Cooldown Tracker",
-//            description = "Shows a display with your hotbar items' cooldowns on each item",
-//            category = "Miscellaneous",
-//            subcategory = "Items",
-//            isParent = true
-//    )
-//    var cooldownTracker = true
-//
-//    @ConfigProperty(
-//            type = ConfigType.DROPDOWN,
-//            name = "Cooldown Display Type",
-//            description = "Draws a square behind items that are currently on cooldown",
-//            category = "Miscellaneous",
-//            subcategory = "Items",
-//            parentName = "Cooldown Tracker",
-//            dropdownOptions = ["Slot Background", "Item Bar"]
-//    )
-//    var cooldownTrackerType = "Item bar"
-//
-//    @ConfigProperty(
-//            type = ConfigType.COLOR,
-//            name = "Background Color",
-//            description = "",
-//            category = "Miscellaneous",
-//            subcategory = "Items",
-//            parentName = "Cooldown Tracker"
-//    )
-//    var cooldownTrackerSquareColor = Color.white
-//
-//    @ConfigProperty(
-//            type = ConfigType.COLOR,
-//            name = "Bar Color",
-//            description = "",
-//            category = "Miscellaneous",
-//            subcategory = "Items",
-//            parentName = "Cooldown Tracker"
-//    )
-//    var cooldownTrackerBarColor = Color.cyan
-//
 //    @ConfigProperty(
 //            type = ConfigType.TOGGLE,
 //            name = "Show Estimated Value",
--- src/main/java/mrfast/sbt/features/mining/dwarvenmines/GemstoneGrinderOverlay.kt
+++ src/main/java/mrfast/sbt/features/mining/dwarvenmines/GemstoneGrinderOverlay.kt
@@ -36,20 +36,18 @@ object GemstoneGrinderOverlay {
         override fun draw(mouseX: Int, mouseY: Int, event: Event) {
             if (event !is GuiContainerBackgroundDrawnEvent) return
 
-            val gemstones = mutableListOf<String>()
+            val gemstones = mutableListOf<Pair<String, List<String>>>()
+
             for (i in 27..34) {
                 val gemstone = event.gui!!.inventorySlots.getSlot(i)
                 if (gemstone.stack != null) {
-                    if (gemstone.stack.displayName.clean().trim()
-                            .isEmpty() || gemstone.stack.displayName.clean() == "Gemstone Slot"
-                    ) continue
+                    if (gemstone.stack.displayName.clean().trim().isEmpty() || gemstone.stack.displayName.clean() == "Gemstone Slot") continue
 
-                    gemstones.add(gemstone.stack.displayName)
                     val temp = mutableListOf<String>()
-
                     var isCostSection = false
+                    var totalPrice = 0.0
 
-                    // Already bought slot thats empty
+                    // Already bought slot that's empty
                     if (gemstone.stack.getLore(false).size < 5) {
                         temp.add("§7Unused Slot")
                         temp.add(" ")
@@ -69,27 +67,45 @@ object GemstoneGrinderOverlay {
                         if (isCostSection) {
                             val cleanedLine = line.clean().trim()
                             if (cleanedLine.isEmpty() || line.contains("Click to unlock!", ignoreCase = true)) {
-                                temp.add("")
                                 break@lore // Exit if the line is empty or irrelevant
                             }
                             if (cleanedLine.contains("Coins")) {
+                                val coins = cleanedLine.replace("[^0-9]".toRegex(), "").toDoubleOrNull() ?: 0.0
+                                totalPrice += coins
                                 temp.add(line)
 
                                 if (temp[temp.size - 2].contains("Remove")) {
-                                    temp.add("")
+                                    totalPrice *= -1
                                 }
                             } else {
                                 val split = cleanedLine.split("x")
                                 val count = if (split.size > 1) split[1].trim().toInt() else 1
                                 val itemId = ItemApi.getItemIdFromName(split[0].trim(), true) ?: continue
                                 val price = ItemApi.getItemInfo(itemId)?.get("bazaarBuy")?.asDouble ?: continue
+                                totalPrice += count * price
                                 temp.add(line.replace(" Gemstone", "") + " §7- §6${(count * price).abbreviateNumber()}")
                             }
                         }
                     }
 
-                    // Add only non-blank lines to the gemstones list
-                    gemstones.addAll(temp)
+                    // Add gemstone with its cost information and total price in the title
+                    var gemstoneTitle = gemstone.stack.displayName
+                    if (gemstoneTitle.contains("Slot")) {
+                        gemstoneTitle = gemstoneTitle.replace(" Gemstone", "")
+                    }
+                    if (totalPrice > 0) {
+                        gemstoneTitle += " §7- Total: §6${totalPrice.abbreviateNumber()}"
+                    } else if (totalPrice == 0.0) {
+                        gemstoneTitle += " §7- §cEmpty"
+                    }
+
+                    gemstones.add(gemstoneTitle to listOf())
+                    if (totalPrice > 0) {
+                        gemstones.add("§7[Hover for slot details]" to temp)
+                    } else if (totalPrice < 0) {
+                        gemstones.add("§7Cost to remove - §6${(totalPrice * -1).abbreviateNumber()}" to temp)
+                    }
+                    gemstones.add("" to listOf())
                 }
             }
 
@@ -97,15 +113,11 @@ object GemstoneGrinderOverlay {
 
             val lines = mutableListOf<GuiUtils.Element>()
 
-            gemstones.forEach {
-                lines.add(GuiUtils.Element(5f, 5f + lines.size * 10, it, null, null))
+            gemstones.forEach { (gemstone, cost) ->
+                lines.add(GuiUtils.Element(5f, 5f + lines.size * 10, gemstone, cost.ifEmpty { null }, null))
             }
 
-
-            val width =
-                (lines.sortedBy { it.text.getStringWidth() }[lines.size - 1].text.getStringWidth() + 15).coerceIn(
-                    100, 200
-                )
+            val width = (lines.sortedBy { it.text.getStringWidth() }[lines.size - 1].text.getStringWidth() + 15).coerceIn(100, 200)
 
             // Change z-depth in order to be above NEU inventory buttons
             GlStateManager.translate(0f, 0f, 52f)
@@ -114,7 +126,7 @@ object GemstoneGrinderOverlay {
                 2f,
                 0f,
                 width.toFloat(),
-                (10 + 9.4 * lines.size).toFloat(),
+                (10 + 9.2 * lines.size).toFloat(),
                 4f,
                 Color(18, 18, 18),
                 GuiUtils.rainbowColor.get().constraint,
